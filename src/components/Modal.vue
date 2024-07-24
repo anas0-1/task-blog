db22df8c5555670f0ff9663e85117555eab7c21d
anas0-1 <anaszrigui01@gmail.com>
2024-07-24 10:08:45 +0100

    .

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center">
-    <div class="bg-white p-8 rounded shadow-lg w-full max-w-md">
-      <button class="absolute top-0 right-0 m-4 text-gray-500" @click="$emit('close')">&times;</button>
-      <slot></slot>
+  <div class="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center z-50">
+    <div class="bg-white rounded-lg p-6 w-1/2">
+      <div class="flex justify-end">
+        <button @click="$emit('close')" class="text-gray-600 hover:text-gray-900">&times;</button>
+      </div>
+      <div>
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AppModal'
+  name: 'AppModal',
 };
 </script>
 
 <style scoped>
-/* Add styles here */
 </style>
